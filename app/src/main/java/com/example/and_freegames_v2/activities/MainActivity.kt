27dc81c727566c6077e243697a3282a6bd90cf73
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-25 21:07:53 +0200

    Game.kt and MainAdapter.kt added

--- app/src/main/java/com/example/and_freegames_v2/activities/MainActivity.kt
+++ app/src/main/java/com/example/and_freegames_v2/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.and_freegames_v2
+package com.example.and_freegames_v2.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.and_freegames_v2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/and_freegames_v2/model/Game.kt
@@ -0,0 +1,4 @@
+package com.example.and_freegames_v2.model
+
+class Game {
+}
